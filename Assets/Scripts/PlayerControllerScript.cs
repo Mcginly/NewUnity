656d33e9a2de3758d9270263d5f762efe93bfda1
McGinly <indifikat@gmail.com>
2020-03-26 22:22:03 +0300

    Create

---
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveH, moveV);
+        rb2d.AddForce(movement * speed);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
